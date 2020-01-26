12eb4033a748a18e57776469963617aab3e6d8cb
Susana Pham <s.susana.pham@gmail.com>
2020-01-26 14:16:31 +0100

    Add more style and clean up

--- app/components/contact.js
+++ app/components/contact.js
@@ -13,20 +13,19 @@ module.exports = async function($) {
   async function sendEmail(form) {
     var button =  q('.form-button')
     button.disabled = true
-    var buttonHTML = html(button)
-    html(button, '...')
-    var data = serialize(form)
-    const result = await api.fetch({ path: 'sendEmail', data })
+
+  // Sleep 2 seconds
+  await new Promise(r => setTimeout(r, 1000))
+
+    const result = await api.fetch({ path: 'sendEmail', data: serialize(form) })
 
     if (result.error) {
       css('.message.error', 'opacity: 1')
       window.location = '#kontakt'
-
       Object.keys(result.error.data).forEach(function(key) {
         text(`.${key}-error`, result.error.data[key].join(', '))
       })
       button.disabled = false
-      html(button, buttonHTML)
     } else {
       form.reset()
       window.location = $.link('bekreftelse')
@@ -63,15 +62,15 @@ module.exports = async function($) {
                 <div class="col col-6">
                   <div class="form-item">
                     <label>${ $.t('index_kontakt_form1') }<span class="req">*</span></label>
-                    <input type="text" name="name" onfocus="clearField(this)">
-                    <span class="form-error name-error"></span>
+                    <input type="text" name="name" placeholder="Hans Hansen" onfocus="clearField(this)">
+                    <span class="form-error name-error error-message"></span>
                   </div>
                 </div>
                 <div class="col col-6">
                   <div class="form-item">
                     <label>${ $.t('index_kontakt_form2') }</label>
-                    <input type="text" name="phone" onfocus="clearField(this)">
-                    <span class="form-error phone-error"></span>
+                    <input type="text" name="phone" placeholder="22222222" onfocus="clearField(this)">
+                    <span class="form-error phone-error error-message"></span>
                   </div>
                 </div>
               </div>
@@ -79,28 +78,31 @@ module.exports = async function($) {
                 <div class="col col-6">
                   <div class="form-item">
                     <label>${ $.t('index_kontakt_form3') }<span class="req">*</span></label>
-                    <input type="text" name="email">
-                    <span class="form-error email-error"></span>
+                    <input type="text" name="email" placeholder="mail@example.com" onfocus="clearField(this)">
+                    <span class="form-error email-error error-message"></span>
                   </div>
                 </div>
                 <div class="col col-6">
                   <div class="form-item">
                     <label>${ $.t('index_kontakt_form4') }<span class="req">*</span></label>
-                    <select name="subject">
+                    <select name="subject" onfocus="clearField(this)">
                       <option value="">---</option>
                       ${ renderSubjectOptions() }
                     </select>
-                    <span class="form-error subject-error"></span>
+                    <span class="form-error subject-error error-message"></span>
                   </div>
                 </div>
               </div>
               <div class="form-item">
                 <label>${ $.t('index_kontakt_form5') }</label>
-                <textarea rows="6" name="message"></textarea>
-                <span class="form-error message-error"></span>
+                <textarea rows="6" name="message" placeholder="Skriv beskjed her" onfocus="clearField(this)"></textarea>
+                <span class="form-error message-error error-message"></span>
               </div>
               <div class="form-item">
-                <button class="form-button w100 button-style">Send</button>
+                <button class="form-button w100 button-style">
+                  <span>Send</span>
+                  <img class="loader" src="/images/loader.svg">
+                </button>
               </div>
             </form>
           </div>
