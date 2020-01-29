f4d68748ac23ad6ea6c71fe80013db75f3ddb04b
Vidar Eldøy <vidar@eldoy.com>
2020-01-29 07:07:52 +0100

    Build host

--- app/layouts/default.js
+++ app/layouts/default.js
@@ -1,7 +1,7 @@
 module.exports = async function($) {
-  const host = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://speria.no'
+  const host = $.req.headers['x-waveorb']
+    ? 'https://speria.no'
+    : 'http://localhost:5000'
 
   function init() {
     document.querySelectorAll('nav a').forEach(function(a) {
