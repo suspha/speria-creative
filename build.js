78f79bc966abc8fb7fe311a0412991c2d4f94ff4
Vidar Eldøy <vidar@eldoy.com>
2021-03-07 09:04:04 +0100

    No need for build file

--- build.js
+++
@@ -1,6 +0,0 @@
-const loader = require('conficurse')
-const config = loader.load('app/config')
-const urls = Object.keys(config.routes.routemap)
-module.exports = async function() {
-  return { host: 'http://localhost:5000', urls }
-}
