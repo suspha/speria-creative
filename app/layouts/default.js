a6a62cc0e630e1e9cc757f59be3f61dc738ee294
Vidar Eldøy <vidar@eldoy.com>
2021-03-13 17:04:24 +0100

    Remove google analytics

--- app/layouts/default.js
+++ app/layouts/default.js
@@ -105,15 +105,6 @@ module.exports = async function($) {
           <p>2020 Design and code by Speria Creative</p>
         </footer>
         <script>cookie('lang', '${ $.lang }')</script>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-100485356-1', 'auto');
-          ga('send', 'pageview');
-        </script>
       </body>
     </html>
 
